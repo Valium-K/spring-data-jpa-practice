096b745be8e00d83aea6b0486afe878868ad94d4
Valium-K <lschan96@gmail.com>
2021-07-28 17:23:42 +0900

    기본적인 crud를 직접 작성

--- src/main/java/dev/valium/springdatajpaprectice/repository/MemberJpaRepository.java
+++ src/main/java/dev/valium/springdatajpaprectice/repository/MemberJpaRepository.java
@@ -6,6 +6,8 @@ import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 @RequiredArgsConstructor
@@ -20,5 +22,21 @@ public class MemberJpaRepository {
 
     public Member find(Long id) {
         return em.find(Member.class, id);
+    }
+
+    public List<Member> findAll() {
+        return em.createQuery(
+                "select m from Member m", Member.class
+        ).getResultList();
+    }
+
+    public Optional<Member> findById(Long id) {
+        return Optional.ofNullable(find(id));
+    }
+
+    public long count() {
+        return em.createQuery(
+                "select count(m) from Member m", Long.class)
+                .getSingleResult();
     }
 }
