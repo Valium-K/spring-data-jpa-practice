ed2084bb7ba39e919bbd37a657606ae1b3e46119
Valium-K <lschan96@gmail.com>
2021-07-28 16:44:56 +0900

    jpa 매핑 추가

--- src/main/java/dev/valium/springdatajpaprectice/entity/Member.java
+++ src/main/java/dev/valium/springdatajpaprectice/entity/Member.java
@@ -1,23 +1,33 @@
 package dev.valium.springdatajpaprectice.entity;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.List;
 
 @Entity
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString(of = {"id", "userName", "age"}) // team 넣으면 순환참조에 의해 무한루프 가능성
 public class Member {
 
     @Id @GeneratedValue
+    @Column(name = "member_id")
     private Long id;
 
     private String userName;
+    private int age;
 
-    protected Member() {}
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "team_id")
+    private Team team;
+
     public Member(String userName) {
         this.userName = userName;
+    }
+
+    public void changeTeam(Team team) {
+        this.team = team; // 해당 member의 team 연관관계 설정
+        team.getMembers().add(this); // team의 member도 연관관계 설정
     }
 }
