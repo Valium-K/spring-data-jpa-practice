811f2dcb3051da0fa8c2d110f50cc2b5e5b39e36
Valium-K <lschan96@gmail.com>
2021-07-29 17:31:36 +0900

    메소드 이름 쿼리, 레포지토리 @Query 추가

--- src/main/java/dev/valium/springdatajpaprectice/repository/MemberRepository.java
+++ src/main/java/dev/valium/springdatajpaprectice/repository/MemberRepository.java
@@ -2,7 +2,19 @@ package dev.valium.springdatajpaprectice.repository;
 
 import dev.valium.springdatajpaprectice.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
+    // 메소드 이름으로 쿼리 생성
+    List<Member> findByUserNameAndAgeGreaterThan(String username, int age);
+
+    // 레포지토리 메소드
+    @Query("select m from Member m where m.userName = :userName and m.age > :age")
+    List<Member> This_method_and_upper_method_is_exactly_the_same(
+            @Param("userName") String username,
+            @Param("age") int age);
 }
